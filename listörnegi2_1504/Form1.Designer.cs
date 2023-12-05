254341b838aaef95b0cda6128ba3dae339a8698d
akinseman <145451296+akinseman@users.noreply.github.com>
2023-12-05 12:34:42 +0300

    Add files via upload

---
+++ listörnegi2_1504/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace listörnegi2_1504
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnkişiler = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.btnara = new System.Windows.Forms.Button();
+            this.btnsıra = new System.Windows.Forms.Button();
+            this.btnsılı = new System.Windows.Forms.Button();
+            this.bttnremov = new System.Windows.Forms.Button();
+            this.btnkayıt = new System.Windows.Forms.Button();
+            this.btndunezenle = new System.Windows.Forms.Button();
+            this.btnartan = new System.Windows.Forms.Button();
+            this.btnazalan = new System.Windows.Forms.Button();
+            this.btn5harlıgörenci = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnkişiler
+            // 
+            this.btnkişiler.Location = new System.Drawing.Point(12, 3);
+            this.btnkişiler.Name = "btnkişiler";
+            this.btnkişiler.Size = new System.Drawing.Size(75, 50);
+            this.btnkişiler.TabIndex = 0;
+            this.btnkişiler.Text = "kişiler";
+            this.btnkişiler.UseVisualStyleBackColor = true;
+            this.btnkişiler.Click += new System.EventHandler(this.btnkişiler_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(159, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(208, 251);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(146, 19);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 2;
+            // 
+            // btnara
+            // 
+            this.btnara.Location = new System.Drawing.Point(12, 59);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(75, 61);
+            this.btnara.TabIndex = 3;
+            this.btnara.Text = "ara";
+            this.btnara.UseVisualStyleBackColor = true;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // btnsıra
+            // 
+            this.btnsıra.Location = new System.Drawing.Point(12, 126);
+            this.btnsıra.Name = "btnsıra";
+            this.btnsıra.Size = new System.Drawing.Size(75, 87);
+            this.btnsıra.TabIndex = 4;
+            this.btnsıra.Text = "saıra";
+            this.btnsıra.UseVisualStyleBackColor = true;
+            this.btnsıra.Click += new System.EventHandler(this.btnsıra_Click);
+            // 
+            // btnsılı
+            // 
+            this.btnsılı.Location = new System.Drawing.Point(12, 219);
+            this.btnsılı.Name = "btnsılı";
+            this.btnsılı.Size = new System.Drawing.Size(75, 66);
+            this.btnsılı.TabIndex = 5;
+            this.btnsılı.Text = "Sili";
+            this.btnsılı.UseVisualStyleBackColor = true;
+            this.btnsılı.Click += new System.EventHandler(this.btnsılı_Click);
+            // 
+            // bttnremov
+            // 
+            this.bttnremov.Location = new System.Drawing.Point(12, 272);
+            this.bttnremov.Name = "bttnremov";
+            this.bttnremov.Size = new System.Drawing.Size(75, 42);
+            this.bttnremov.TabIndex = 6;
+            this.bttnremov.Text = "ronov ile sil";
+            this.bttnremov.UseVisualStyleBackColor = true;
+            this.bttnremov.Click += new System.EventHandler(this.bttnremov_Click);
+            // 
+            // btnkayıt
+            // 
+            this.btnkayıt.Location = new System.Drawing.Point(12, 320);
+            this.btnkayıt.Name = "btnkayıt";
+            this.btnkayıt.Size = new System.Drawing.Size(75, 70);
+            this.btnkayıt.TabIndex = 7;
+            this.btnkayıt.Text = "kayıt";
+            this.btnkayıt.UseVisualStyleBackColor = true;
+            this.btnkayıt.Click += new System.EventHandler(this.btnkayıt_Click);
+            // 
+            // btndunezenle
+            // 
+            this.btndunezenle.Location = new System.Drawing.Point(12, 396);
+            this.btndunezenle.Name = "btndunezenle";
+            this.btndunezenle.Size = new System.Drawing.Size(75, 43);
+            this.btndunezenle.TabIndex = 8;
+            this.btndunezenle.Text = "DÜZENLE";
+            this.btndunezenle.UseVisualStyleBackColor = true;
+            this.btndunezenle.Click += new System.EventHandler(this.btndunezenle_Click);
+            // 
+            // btnartan
+            // 
+            this.btnartan.Location = new System.Drawing.Point(12, 445);
+            this.btnartan.Name = "btnartan";
+            this.btnartan.Size = new System.Drawing.Size(75, 23);
+            this.btnartan.TabIndex = 9;
+            this.btnartan.Text = "artan";
+            this.btnartan.UseVisualStyleBackColor = true;
+            this.btnartan.Click += new System.EventHandler(this.btnartan_Click);
+            // 
+            // btnazalan
+            // 
+            this.btnazalan.Location = new System.Drawing.Point(12, 474);
+            this.btnazalan.Name = "btnazalan";
+            this.btnazalan.Size = new System.Drawing.Size(75, 24);
+            this.btnazalan.TabIndex = 10;
+            this.btnazalan.Text = "azaln";
+            this.btnazalan.UseVisualStyleBackColor = true;
+            this.btnazalan.Click += new System.EventHandler(this.btnazalan_Click);
+            // 
+            // btn5harlıgörenci
+            // 
+            this.btn5harlıgörenci.Location = new System.Drawing.Point(13, 505);
+            this.btn5harlıgörenci.Name = "btn5harlıgörenci";
+            this.btn5harlıgörenci.Size = new System.Drawing.Size(110, 48);
+            this.btn5harlıgörenci.TabIndex = 11;
+            this.btn5harlıgörenci.Text = "5 harlı görenici";
+            this.btn5harlıgörenci.UseVisualStyleBackColor = true;
+            this.btn5harlıgörenci.Click += new System.EventHandler(this.btn5harlıgörenci_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 619);
+            this.Controls.Add(this.btn5harlıgörenci);
+            this.Controls.Add(this.btnazalan);
+            this.Controls.Add(this.btnartan);
+            this.Controls.Add(this.btndunezenle);
+            this.Controls.Add(this.btnkayıt);
+            this.Controls.Add(this.bttnremov);
+            this.Controls.Add(this.btnsılı);
+            this.Controls.Add(this.btnsıra);
+            this.Controls.Add(this.btnara);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnkişiler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnkişiler;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.Button btnsıra;
+        private System.Windows.Forms.Button btnsılı;
+        private System.Windows.Forms.Button bttnremov;
+        private System.Windows.Forms.Button btnkayıt;
+        private System.Windows.Forms.Button btndunezenle;
+        private System.Windows.Forms.Button btnartan;
+        private System.Windows.Forms.Button btnazalan;
+        private System.Windows.Forms.Button btn5harlıgörenci;
+    }
+}
+
---
+++ listörnegi2_1504/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listörnegi2_1504
+{
+    public partial class Form1 : Form
+    {List<string> kişiler= new List<string>();
+      int indexSira=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kişiler.Add("Semanur");
+            kişiler.Add("Melisa");
+            kişiler.Add("sükanya");
+            kişiler.Add("ravza");
+            kişiler.Add("hilila");
+            kişiler.Add("irem");
+    
+
+            listBox1.DataSource = kişiler.ToList();
+        }
+
+        private void btnkişiler_Click(object sender, EventArgs e)
+        {
+            kişiler.Add (txtad.Text);
+
+            listBox1.DataSource=kişiler.ToList ();
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+            bool durum = kişiler.Contains(txtad.Text);
+
+            if (durum)
+            {
+                MessageBox.Show("bu kişiler var");
+
+            }
+            else
+            {
+                MessageBox.Show("bu kişi yok"); 
+
+            }
+        }
+
+        private void btnsıra_Click(object sender, EventArgs e)
+        {
+            int sira = kişiler.IndexOf(txtad.Text);
+          if(sira > -1)
+            {
+                MessageBox.Show(txtad.Text + " " + sira + "sırasında");
+
+            }
+            else
+            {
+                MessageBox.Show(txtad.Text + "sıralamada yok");
+
+
+            }
+
+        }
+
+        private void btnsılı_Click(object sender, EventArgs e)
+        {
+            int sira = kişiler.IndexOf(txtad.Text);
+            if(sira < -1)
+            {
+                kişiler.RemoveAt(sira);
+
+            } listBox1.DataSource =kişiler.ToList ();
+        }
+
+        private void bttnremov_Click(object sender, EventArgs e)
+        {
+            bool durum = kişiler.Contains(txtad.Text);
+
+            if (durum)
+            {
+                kişiler.Remove(txtad.Text);
+
+            } listBox1.DataSource = kişiler.ToList();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtad.Text = listBox1.SelectedValue.ToString();
+           indexSira = listBox1.SelectedIndex;
+        }
+
+        private void btnkayıt_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(indexSira.ToString());
+        }
+
+        private void btndunezenle_Click(object sender, EventArgs e)
+        {
+            kişiler[indexSira] = txtad.Text;
+            listBox1.DataSource = kişiler.ToList();
+        }
+
+        private void btnartan_Click(object sender, EventArgs e)
+        {
+            kişiler.Sort();
+            
+            listBox1.DataSource= kişiler.ToList() ;
+
+        }
+
+        private void btnazalan_Click(object sender, EventArgs e)
+        {
+            kişiler.Sort();
+            kişiler.Reverse();
+
+            listBox1.DataSource = kişiler.ToList(); 
+        }
+
+        private void btn5harlıgörenci_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 0; i < kişiler.Count; i++)
+            {
+              if (kişiler[i].Length == 5)
+                {
+                    sayac++;
+                }
+
+                    
+
+            }MessageBox.Show("5 karekkterli:" + "sayac" + "tan kişiyi göster");
+        }
+
+    }
+}
